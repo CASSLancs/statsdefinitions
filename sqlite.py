4fb73ec99fabc434feafdcdc59e9308c30651514
Jack Dunn <56315967+JackDunnCodes@users.noreply.github.com>
2023-08-05 13:41:37 +0100

    Ensure term is cleared out

--- sqlite.py
+++ sqlite.py
@@ -40,6 +40,7 @@ for fname in termFiles:
     with open(os.path.join(termsDir, fname), encoding="utf8") as f:
         termData = f.read()
 
+    term = {}
     term = json.loads(termData)
     definitions.append((
         term["identifier"], term["name"], term.get("alternateName", None),
